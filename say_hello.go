6afbca7a828e1918e759456ff1d07f582838f5df
AdrianProgramer <adrianfahren04@gmail.com>
2022-02-10 14:45:21 +0700

    update 2.0.0

--- say_hello.go
+++ say_hello.go
@@ -3,3 +3,15 @@ package module_go
 func SayHello(name string) string {
 	return name
 }
+
+func KalkulatorTambah(number1 int, number2 int) int {
+	return number1 + number2
+}
+
+func KalkulatorKurang(number1 int, number2 int) int {
+	return number1 - number2
+}
+
+func KalkulatorBagi(number1 int, number2 int) int {
+	return number1 / number2
+}
