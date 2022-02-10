4b261cfe0f1e5b05b3ffd4a2899575d8d3abe367
AdrianProgramer <adrianfahren04@gmail.com>
2022-02-10 16:19:08 +0700

    revisi

--- say_hello.go
+++ say_hello.go
@@ -1,7 +1,5 @@
 package module_go
 
-import "fmt"
-
 func SayHello(name string) string {
 	return name
 }
@@ -16,16 +14,4 @@ func KalkulatorKurang(number1 int, number2 int) int {
 
 func KalkulatorBagi(number1 int, number2 int) int {
 	return number1 / number2
-}
-
-func loopS(loopstring [...]string) {
-	for i := 0; i < len(loopstring); i++ {
-		fmt.Println(loopstring[i])
-	}
-}
-
-func loopI(loopint [...]int) {
-	for i := 0; i < len(loopint); i++ {
-		fmt.Println(loopint[i])
-	}
 }
