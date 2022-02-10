66c3ebdc1cd8f5162a556eb35fc92ba6ac9bef3e
AdrianProgramer <adrianfahren04@gmail.com>
2022-02-10 18:15:45 +0700

    V2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/AdrianProgramer/say-hello-module
+module github.com/AdrianProgramer/say-hello-module/v2
 
 go 1.17
--- say_hello.go
+++ say_hello.go
@@ -4,18 +4,18 @@ func SayHello(name string) string {
 	return name
 }
 
-func KalkulatorTambah(number1 int, number2 int) int {
-	return number1 + number2
+func KalkulatorTambah(number1 int, number2 int, number3 int) int {
+	return number1 + number2 + number3
 }
 
-func KalkulatorKurang(number1 int, number2 int) int {
-	return number1 - number2
+func KalkulatorKurang(number1 int, number2 int, number3 int) int {
+	return number1 - number2 - number3
 }
 
-func KalkulatorBagi(number1 int, number2 int) int {
-	return number1 / number2
+func KalkulatorBagi(number1 int, number2 int, number3 int) int {
+	return number1 / number2 / number3
 }
 
-func Modulus(number1 int, number2 int) int {
+func Modulus(number1 int, number2 int, number3 int) int {
 	return number1 % number2
 }
